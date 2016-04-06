bd2aabcb79c1c2d72b9a1d59caa09f4293aa304d
Eric Fode <eric.fode@autodesk.com>
2016-04-06 14:23:49 -0700

    more user and group things

--- libraries/vault_config.rb
+++ libraries/vault_config.rb
@@ -86,6 +86,8 @@ module VaultCookbook
         notifying_block do
           directory ::File.dirname(new_resource.path) do
             recursive true
+            owner new_resource.owner
+            group new_resource.group
           end
 
           file new_resource.path do
